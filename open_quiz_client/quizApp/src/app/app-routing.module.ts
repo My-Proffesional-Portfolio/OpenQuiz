514f991b6b3c31e2f10e17d32de727f593cd9c85
Rolando <rolando.code@gmail.com>
2023-01-06 15:30:23 -0600

    Button go to quiz component

--- open_quiz_client/quizApp/src/app/app-routing.module.ts
+++ open_quiz_client/quizApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizViewComponent } from './Components/quiz-view/quiz-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'quiz', component: QuizViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
